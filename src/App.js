e608e57cac680101cfef43c0ed4098c3f3b6eb7b
Renata Marques <renata.nrmarques@gmail.com>
2020-04-23 00:33:11 -0300

    todo component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'italki lesson',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'exercises',
+        completed: true
+      }
+    ]
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
+}
 export default App;
